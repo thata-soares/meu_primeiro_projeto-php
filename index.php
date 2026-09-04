cdf64e3fb22e7a7dd6fe2c97b1d6fc444e89363d
THALYTA SOARES DE ANDRADE SILVA <thalyta.silva6@senaisp.edu.br>
2026-09-04 10:32:08 -0300

    curso adicionado

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 $nome = "Thalyta Soares de Andrade Silva";
 $profissao = "Auxiliar de Recursos Humanos";
+$curso = "Análise e Desenvolvimento de Sistemas";
 $mensagem = "Meu primeiro projeto em PHP com Git e Github";
 
 ?>
@@ -18,6 +19,8 @@ $mensagem = "Meu primeiro projeto em PHP com Git e Github";
     
     <h2><?php echo $profissao; ?><h2>
 
+    <h2><?php echo  $curso; ?><h2>
+        
     <p><?php echo $mensagem; ?></p>
 </body>
 </html>
