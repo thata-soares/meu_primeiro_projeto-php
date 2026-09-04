421b4f603910c3fe8effd4eca633c707efce9bbd
THALYTA SOARES DE ANDRADE SILVA <thalyta.silva6@senaisp.edu.br>
2026-09-04 09:45:26 -0300

    criação projeto inicial em PHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$nome = "Thalyta Soares de Andrade Silva";
+$profissao = "Auxiliar de Recursos Humanos";
+$mensagem = "Meu primeiro projeto em PHP com Git e Github";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto PHP - Thalyta Soares</title>
+</head>
+<body>
+    <h1><?php echo $nome; ?><h1>
+    
+    <h2><?php echo $profissao; ?><h2>
+
+    <p><?php echo $mensagem; ?></p>
+</body>
+</html>
